28b91ebc4c9ec49f0c7ea873299ccaf510bf39bf
Leonardo Pinheiro <leo.pinnheiro@gmail.com>
2019-09-24 01:19:33 -0300

    Criando funcionalidade de adicionar ao carrinho com redux

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import { MdAddShoppingCart } from 'react-icons/md';
 import { formatPrice } from '../../util/format';
 import api from '../../services/api';
 
 import { ProductList } from './styles';
 
-export default class Home extends Component {
+class Home extends Component {
+  // eslint-disable-next-line react/state-in-constructor
   state = {
     products: [],
   };
@@ -20,6 +22,15 @@ export default class Home extends Component {
     this.setState({ products: data });
   }
 
+  handleAddProduct = product => {
+    const { dispatch } = this.props;
+
+    dispatch({
+      type: 'ADD_TO_CART',
+      product,
+    });
+  };
+
   render() {
     const { products } = this.state;
 
@@ -30,7 +41,10 @@ export default class Home extends Component {
             <img src={product.image} alt={product.title} />
             <strong>{product.title}</strong>
             <span>{product.priceFormatted}</span>
-            <button type="button">
+            <button
+              type="button"
+              onClick={() => this.handleAddProduct(product)}
+            >
               <div>
                 <MdAddShoppingCart size={16} color="#fff" /> 3
               </div>
@@ -42,3 +56,5 @@ export default class Home extends Component {
     );
   }
 }
+
+export default connect()(Home);
